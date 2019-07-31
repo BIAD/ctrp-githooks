e3ac0fdf6c89f05bd0d523a71b175692714b44d1
jackyu-it <jackyu.it@gmail.com>
2019-07-31 14:27:56 -0400

    updated hook

--- .githooks/python/pre-commit.sh
+++ .githooks/python/pre-commit.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
 # A pre-commit hook to run Pylint on all files. Use the "pylintrc" config file.
-# It exclude these folder: venv, virtualenv, lib
+# It exclude these folder: venv, env, lib
 #
 
 
 echo "pre-commit: Running lint check"
-find . -type d \( -path ./venv -o -path ./virtualenv -o -path ./lib \) -prune -o -iname '*.py' | xargs pylint --ignore=venv --ignore=virtualenv --ignore=lib --rcfile=pylintrc
+find . -type d \( -path ./venv -o -path ./env -o -path ./lib \) -prune -o -iname '*.py' | xargs pylint --ignore=venv --ignore=env --ignore=lib --rcfile=pylintrc
 
